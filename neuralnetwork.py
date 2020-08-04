225402e4b236ed9f522b8df8cea79582c9185760
Seseel Lybon <seseellybon@gmail.com>
2020-08-04 20:54:29 +0200

    Ported NN.crossover to TF/K

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -84,19 +84,9 @@ class NeuralNetwork:
     def clone(self)->tf.keras.Model:
         clone = tf.keras.models.clone_model(self.model)
 
-        if self.hidden_size[0] == 0:
-            # if [0]==0, there are no hidden layers to deal with
-            clone.layers[0].set_weights(self.model.layers[0].get_weights() )
-        else:
-            # clone first layer
-            clone.layers[0].set_weights(self.model.layers[0].get_weights() )
-
-            # clone hidden layers (except the first one)
-            for hz_i in range(1, len(self.hidden_size)):
-                clone.layers[hz_i].set_weights(self.model.layers[hz_i].get_weights() )
-
-            # clone final layer
-            clone.layers[-1].set_weights(self.model.layers[-1].get_weights() )
+        # clone layers
+        for hz_i in range(len(self.hidden_size)+1):
+            clone.layers[hz_i].set_weights(self.model.layers[hz_i].get_weights() )
 
         clone.compile(optimizer=self.optimizer,
                       loss=self.loss,
@@ -111,67 +101,81 @@ class NeuralNetwork:
     def mutate(self,mutatechance=1/30, mutatestrength=1):
         ms_half = mutatestrength/2
 
-        if self.hidden_size[0] == 0:
-            # mutate the output layer
-            temp = self.model.layers[0].get_weights()
-            for owi in range(self.output_size): # owi; output weight index
-                if np.random.rand() <= mutatechance:
-                    temp[0][0][owi] += np.random.uniform(-ms_half,ms_half)
-                    temp[1][owi] += np.random.uniform(-ms_half,ms_half)
-            self.model.layers[0].set_weights(temp)
-
-        else:
+        if self.hidden_size[0] != 0:
             # mutate the hidden layers
-            for hwi in range(len(self.hidden_size)):
-                temp = self.model.layers[hwi].get_weights()
-                for owi in range(self.hidden_size[hwi]): # owi; output weight index
+            for hli in range(len(self.hidden_size)): # hli: hidden layer index
+                temp = self.model.layers[hli].get_weights()
+                for owi in range( self.hidden_size[hli] ): # owi; output weight index
                     if np.random.rand() <= mutatechance:
                         temp[0][0][owi] += np.random.uniform(-ms_half,ms_half)
                         temp[1][owi] += np.random.uniform(-ms_half,ms_half)
-                self.model.layers[hwi].set_weights(temp)
+                self.model.layers[hli].set_weights(temp)
 
-            temp = self.model.layers[-1].get_weights()
-            for owi in range(self.output_size): # owi; output weight index
-                if np.random.rand() <= mutatechance:
-                    temp[0][0][owi] += np.random.uniform(-ms_half,ms_half)
-                    temp[1][owi] += np.random.uniform(-ms_half,ms_half)
-            self.model.layers[-1].set_weights(temp)
-
-
-
+        # mutate the output layer
+        temp = self.model.layers[-1].get_weights()
+        for owi in range(self.output_size):  # owi; output weight index
+            if np.random.rand() <= mutatechance:
+                temp[0][0][owi] += np.random.uniform(-ms_half, ms_half)
+                temp[1][owi] += np.random.uniform(-ms_half, ms_half)
+        self.model.layers[-1].set_weights(temp)
 
 
-    def crossover(self, parent2):
-        child:NeuralNetwork
 
-        if self.hidden_layers[0] is not 0:
-            child = NeuralNetwork(self.input_size - 1,
-                                 tuple([x - 1 for x in self.hidden_size]),
-                                 self.output_size,
-                                 isHollow=True)
-        else:
-            child = NeuralNetwork(self.input_size - 1,
-                                 tuple([0]),
-                                 self.output_size,
-                                 isHollow=True)
+    def crossover(self,
+                  parent2): # parent2: NeuralNetwork
+                    # -> NeuralNetwork
 
-        if self.hidden_layers[0] is not 0:
-            for layeri in range(len(self.hidden_layers)):
-                for nodei in range(self.hidden_layers[layeri].size):
-                    for weighti in range(child.hidden_layers[layeri][nodei].weights.shape[0]):
-                        if np.random.rand() < 0.50:
-                            child.hidden_layers[layeri][nodei].weights[weighti] = self.hidden_layers[layeri][nodei].weights[weighti]
-                        else:
-                            child.hidden_layers[layeri][nodei].weights[weighti] = parent2.hidden_layers[layeri][nodei].weights[weighti]
-        else:
-            child.hidden_layers = [0]
+        child:tf.keras.Model = tf.keras.models.clone_model(self.model)
 
-        for nodei in range(self.output_layer.size):
-            for weighti in range(self.output_layer[nodei].weights.shape[0]):
-                if np.random.rand() < 0.50:
-                    child.output_layer[nodei].weights[weighti] = self.output_layer[nodei].weights[weighti]
-                else:
-                    child.output_layer[nodei].weights[weighti] = parent2.output_layer[nodei].weights[weighti]
+        if self.hidden_size[0] != 0:
+            # crossover the hidden layers
+            for hli in range(len(self.hidden_size)):  # hli: hidden layer index
+                # crossover the output layer
+                temp_P1 = self.model.layers[hli].get_weights()
+                temp_P2 = parent2.model.layers[hli].get_weights()
+                temp_C = child.layers[hli].get_weights()
+                for owi in range(self.hidden_size[hli]):  # owi; output weight index
+                    # copy either the self or parent2 ***weight*** of this weight on this layer into the child
+                    if np.random.rand() <= 0.5:
+                        temp_C[0][0][owi] = temp_P1[0][0][owi]
+                        print("P1-",end="")
+                    else:
+                        temp_C[0][0][owi] = temp_P2[0][0][owi]
+                        print("P2-", end="")
+
+                    # copy either the self or parent2 ***bias*** of this weight on this layer into the child
+                    if np.random.rand() <= 0.5:
+                        temp_C[1][owi] = temp_P1[0][0][owi]
+                    else:
+                        temp_C[1][owi] = temp_P2[0][0][owi]
+
+                child.layers[hli].set_weights(temp_C)
+            print()
+
+        # crossover the output layer
+        temp_P1 = self.model.layers[-1].get_weights()
+        temp_P2 = parent2.model.layers[-1].get_weights()
+        temp_C = child.layers[-1].get_weights()
+        for owi in range(self.output_size): # owi; output weight index
+            # copy either the self or parent2 ***weight*** of this weight on this layer into the child
+            if np.random.rand() <= 0.5:
+                temp_C[0][0][owi] = temp_P1[0][0][owi]
+            else:
+                temp_C[0][0][owi] = temp_P2[0][0][owi]
+
+            # copy either the self or parent2 ***bias*** of this weight on this layer into the child
+            if np.random.rand() <= 0.5:
+                temp_C[1][owi] = temp_P1[1][owi]
+            else:
+                temp_C[1][owi] = temp_P2[1][owi]
+
+            child.layers[-1].set_weights(temp_C)
+
+        child.compile(optimizer=self.optimizer,
+                      loss=self.loss,
+                      metrics=[self.metrics])
+
+        child.build([None, self.input_size])
 
         return child
 
@@ -236,20 +240,13 @@ class NeuralNetwork:
 
     def getAmountWeights(self)->int:
 
-        totalWeights = 0
-
-
-        if self.hidden_layers[0] is not 0:
-            n_layers = [ self.input_layer.size ] + list(self.hidden_size) + [self.output_layer.size]
-
-            for lsizei in range(1, len(n_layers)-1):
-                totalWeights += n_layers[lsizei-1]*(n_layers[lsizei]-1)
-
-            totalWeights += n_layers[-2] * n_layers[-1]
-
+        if self.hidden_size[0] == 0:
+            totalWeights:int = self.output_size*self.input_size
 
         else:
-            totalWeights = self.input_layer.size * self.output_layer.size
+            totalWeights:int = self.output_size*self.hidden_size[-1]
+            for hli in range(len(self.hidden_size)-1, -1 ,-1):# hidden layer index
+                totalWeights += self.hidden_size[-1]*self.hidden_size[hli]
 
         return totalWeights
 
@@ -264,9 +261,11 @@ class NeuralNetwork:
 
 if __name__ == "__main__":
 
+    input_size = 1
+    hidden_size = (5,5,5)
+    output_size = 4
 
-
-    brain1 = NeuralNetwork(1, (5,10), 4)
+    brain1 = NeuralNetwork(input_size, hidden_size, output_size)
 
     train_set = np.array( [[0],[1],[2],[3],[4],[5],[6],[7],[8],[9],[10],[11],[12],[13],[14],[15]] )
     answer_set = np.array( [[0,0,0,0], # 0
@@ -299,60 +298,39 @@ if __name__ == "__main__":
     print(brain1.predict([[11]]))
     print(brain1.predict([[15]]))
 
-    brain2 = NeuralNetwork(1, (5,10), 4, isHollow=True)
+    print("Cloning brain 1 as brain 2")
+    brain2 = NeuralNetwork(input_size, hidden_size, output_size, isHollow=True)
     brain2.model = brain1.clone()
-    print("mutating brain 2 1")
-    brain2.mutate(mutatechance=0.5)
 
     #print("training brain 1")
     #brain2.train(train_set, answer_set, epochs=100, batch_size=16)
 
-    print("predictions of brain 2 1")
+    print("predictions of brain 2")
     print(brain2.predict([[3]]))
     print(brain2.predict([[7]]))
     print(brain2.predict([[11]]))
     print(brain2.predict([[15]]))
 
-    print("mutating brain 2 2")
-    brain2.mutate(mutatechance=0.5)
+    print("mutating brain 2")
+    brain2.mutate(mutatechance=0.9)
     print("predictions of brain 2 2")
     print(brain2.predict([[3]]))
     print(brain2.predict([[7]]))
     print(brain2.predict([[11]]))
     print(brain2.predict([[15]]))
 
+    print("Crossing brain1 and brain2 to brain3")
+    brain3  = NeuralNetwork(input_size, hidden_size, output_size)
+    brain3.model = brain1.crossover(brain2)
 
+    print("predictions of brain 3")
+    print(brain3.predict([[3]]))
+    print(brain3.predict([[7]]))
+    print(brain3.predict([[11]]))
+    print(brain3.predict([[15]]))
 
     #brain1.model.layers[0].set_weights()
-    for mli in range(len(brain1.model.layers)):
+    for mli in range(len(brain1.model.layers)): # mli model layer index
         print("B1",brain1.model.layers[mli].get_weights()[0][0] )
         print("B2",brain2.model.layers[mli].get_weights()[0][0] )
-    print()
-
-
-    test_keras = False
-    if test_keras:
-        model = tf.keras.models.Sequential([
-            tf.keras.layers.Dense(5, input_dim=1, activation=tf.keras.activations.relu)#,
-            #tf.keras.layers.Dense(10, activation='relu'),
-            #tf.keras.layers.Dense(5, activation=tf.keras.activations.relu)
-        ])
-
-        model.compile(optimizer='adam',
-                      loss=tf.keras.losses.poisson,
-                      metrics=['accuracy'])
-
-        train_set = np.array( [[0],[1],[2],[3],[4]] )
-        answer_set = np.array( [[1,0,0,0,0],
-                                [0,1,0,0,0],
-                                [0,0,1,0,0],
-                                [0,0,0,1,0],
-                                [0,0,0,0,1]] )
-
-        model.fit(train_set, answer_set, epochs=100, batch_size=5)
-
-        print( model.predict([[0]]) )
-        print( model.predict([[1]]) )
-        print( model.predict([[2]]) )
-        print( model.predict([[3]]) )
-        print( model.predict([[4]]) )
+        print("B3",brain3.model.layers[mli].get_weights()[0][0] )
