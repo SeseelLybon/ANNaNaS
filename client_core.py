Fixed Random overflow at randint
turns out it's using c-int, not python int. so 2**32 was the max. For now it's 2**64, which should be enough for now?

client_core.py
@@ -136,7 +136,7 @@ def generate_mastermind_solution(t='unique'):
         np.random.shuffle(temp_rng)
         mastermind_solution = temp_rng[:max_pegs]
     else:
-        mastermind_solution = np.random.randint(1, max_dif_pegs+1, max_pegs)
+        mastermind_solution = np.random.randint(1, max_dif_pegs+1, max_pegs, dtype=np.uint64)
     return mastermind_solution
 
 
@@ -160,7 +160,7 @@ def test():
     print("starting run", runi)
     # generate new solution to test all meeps against
 
-    mastermind_solution = np.random.randint(1, max_dif_pegs+1, max_pegs)
+    mastermind_solution = np.random.randint(1, max_dif_pegs+1, max_pegs, dtype=np.uint64)
 
     # run all meeps against this until pop.isDone.
     while not testpop.isDone():

population.py
@@ -540,7 +540,7 @@ def testpopo():
     #hiddensize=tuple([60, 40, 20])
     outputsize=max_pegs*max_dif_pegs
 
-    mastermind_solution = np.random.randint(1, max_dif_pegs+1, max_pegs)
+    mastermind_solution = np.random.randint(1, max_dif_pegs+1, max_pegs, dtype=np.uint64)
 
     meep:Meeple = Meeple(input_size=inputsize, hidden_size=hiddensize, output_size=outputsize,
                              isHallow=False)
@@ -559,7 +559,16 @@ def testpopo():
     meep.results_list.append((attempt, result))
 
 
+
+
 if __name__ == '__main__':
+    print(2**64)
+    print(np.random.randint(0, 2**64, dtype=np.uint64))
+
+
+
+
+if __name__ == '__main__' and False:
     import time
 
     #cur_time = time.time()

species.py
@@ -88,7 +88,7 @@ class Species:
         return float("inf")
 
     def selectParent(self)->Meeple:
-        rand = np.random.randint(0, max(self.fitnessSum, 1))
+        rand = np.random.randint(0, max(self.fitnessSum, 1), dtype=np.uint64)
         runningSum = 0
         for i in range(len(self.meeples)):
             runningSum +=  self.meeples[i].brain.fitness